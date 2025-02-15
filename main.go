c3e802d727893a89096b027719229a05167cd1a4
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2025-02-15 16:09:55 +0100

    Add run function

--- main.go
+++ main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
+	"io/fs"
 	"os"
+	"path/filepath"
 )
 
 // config holds the configurations for the walk CLI.
@@ -33,4 +36,29 @@ func main() {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
+}
+
+// run holds the implementation for the CLI.
+//
+// It walks through the root directory, performing the
+// requested operations on the files and sub-directories
+// it encounter as specified in the cfg parameter.
+func run(root string, out io.Writer, cfg config) error {
+	return filepath.Walk(root, func(path string, info fs.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if filterOut(path, cfg.ext, cfg.size, info) {
+			return nil
+		}
+
+		// If list was explicitly set, don't do anything else.
+		if cfg.list {
+			return listFile(path, out)
+		}
+
+		// List is the default option if nothing else was set.
+		return listFile(path, out)
+	})
 }
